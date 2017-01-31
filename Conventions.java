4fed6b32ff137052fe982bc0d32889961fd70cb8
Kai-Hwa Yao <kaihwayao@gmail.com>
2017-01-30 20:12:01 -0800

    Revisions

--- Conventions.java
+++ Conventions.java
@@ -2,62 +2,52 @@
 // FRC Team 2910 coding conventions for the Java(tm) programming language
 //
 
+/*
+	First, some basic terminology:
+		- A class-level variable is a static variable.
+		- A field is a non-static, member variable of a class or enum.
+		- A constant is a final class-level variable. 
 
-// First, some basic terminology:
-// A class-level variable is a static variable.
-// A field is a non-static, member variable of a class or enum.
-// A constant is a final class-level variable. 
+	Always use curly braces even for single line statements.
+	Curly braces start on the same line.
 
+	Tab size is 4.
 
-// For indentation use tabs and for alignment use spaces
-// Lines should not exceed 120 characters when tabs are treated as 4 spaces
+	Function names should be descriptive
+		- Name a function after the action it performs.
+		- Getters/Setters should start with "get" and "set"
+		- Functions that return a bool should be phrased as a question.
+			 - Ex. isReady(), doesThingExist(), hasThing()
+
+	Use comments and newlines to explain and organize code.
+	Use asserts.
+
+	Avoid using public member variables. Prefer to use Getters/Setters.
+*/
 
 public class ThisIsAClass { // Classes use UpperCamelCase
-	// Order of the parts of a class is as follows.
-	//
-	// * class-level variables
-	//     * public final
-	//     * protected final
-	//     * private final
-	//     * public
-	//     * protected
-	//     * private
-	// * fields
-	//     * public final
-	//     * protected final
-	//     * private final
-	//     * public
-	//     * protected
-	//     * private
-	// * constructors
-	//     * public
-	//     * protected
-	//     * private
-	// * static methods
-	//     * public
-	//     * protected
-	//     * private
-	// * member-methods
-	//     * public final
-	//     * protected final
-	//     * public
-	//     * protected
-	//     * private
-	//
-	// All parts must be ordered alphabetically within their categories.
 
 	public static final double THIS_IS_A_CONSTANT = 3.14; // Constants use UPPERCASE_WITH_UNDERSCORES
 
-	private double m_privateField = 2.71828; // private and protected fields are prefixed with a 'm_' and use 
-	                                         // lowerCamelCase
+	private double mMemberVariable = 2.71828; // All member variables are prefixed with 'm' followed by UpperCamelCase
 
-	public static int getNumber() { // methods use lowerCamelCase
-		return 5;
+	public int getNumber() { // method names are lowerCamelCase
+		int newVariable;		// Declare one variable per line
+		int anotherVariable; 	// Declare one variable per line
+
+		// Always use curly braces even for single-line statements
+		if (foo.memberFunction()) { // Curly braces start on the same line
+			return 0;
+		}
+		else { // Start related control blocks on a new line
+			return 5;
+		}
 	}
 }
 
 public enum ThisIsAnEnum { // Enums use UpperCamelCase
 	ENUM_VALUE, // Enum values use UPPERCASE_WITH_UNDERSCORES
+	ANOTHER_VALUE
 }
 
 public interface IThisIsAnInterface { // Interfaces are prefixed with a 'I' and use UpperCamelCase
